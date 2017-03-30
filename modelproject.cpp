004ff792f89bd988d37b063af84c5903ea5be33a
avogatro <avogatro@gmail.com>
2017-03-30 20:12:35 +0200

    2

---
+++ modelproject.cpp
@@ -0,0 +1,6 @@
+#include "modelproject.h"
+
+ModelProject::ModelProject()
+{
+
+}
---
+++ modelproject.h
@@ -0,0 +1,11 @@
+#ifndef MODELPROJECT_H
+#define MODELPROJECT_H
+
+
+class ModelProject
+{
+public:
+    ModelProject();
+};
+
+#endif // MODELPROJECT_H
